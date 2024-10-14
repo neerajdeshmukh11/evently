294287cf17d8295a64c10512ad4d38aa95a30975
neerajdeshmukh11 <godnvd999@gmail.com>
2024-10-14 12:55:38 +0530

    setting up

---
+++ app/(root)/events/create/page.tsx
@@ -0,0 +1,26 @@
+'use client'
+import EventForm from "@/components/shared/EventForm";
+import { useAuth } from "@clerk/nextjs";
+
+const CreateEvent = () => {
+  const { userId, isLoaded } = useAuth();
+
+  return (
+    <>
+      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+        <h3 className="wrapper h3-bold text-center sm:text-left">Create Event</h3>
+      </section>
+
+      <div className="wrapper my-8">
+        {/* Render the EventForm only when the user is authenticated */}
+        {isLoaded && userId ? (
+          <EventForm userId={userId} type="Create" />
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default CreateEvent;
---
+++ app/(root)/profile/pages/app.js
@@ -0,0 +1,12 @@
+// pages/_app.js
+import { ClerkProvider } from '@clerk/nextjs';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ClerkProvider>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
+}
+
+export default MyApp;
